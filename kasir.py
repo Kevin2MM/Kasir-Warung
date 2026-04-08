a5bd52b790dc2c68216f1d898f588ff23da626b5
k3xciii-oss <k3xciii@gmail.com>
2026-04-08 13:48:13 +0800

    Menambahkan waktu di struk pembayaran

--- kasir.py
+++ kasir.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import os
 
 def header():
@@ -8,52 +10,58 @@ def header():
     print(nama_toko)
     print(alamat_toko)
     print(no_hp)
-    print('-'*30)
+    print('-' * 30)
 
 
 # data menu
 data_menu = {
-    1:('nasi goreng', 12000),
-    2:('nasi padang', 10000),
-    3:('rendang', 15000),
-    4:('bakso', 10000),
+    1: ('nasi goreng', 12000),
+    2: ('nasi padang', 10000),
+    3: ('rendang', 15000),
+    4: ('bakso', 10000),
 }
-# fungsi fungsi yang mau di pakai
-# fungsi untuk menampilkan daftar menu
+
+
+# fungsi menampilkan menu
 def tampil_menu():
-    print('Daftar Menu Warung \n')
+    print('Daftar Menu Warung\n')
     for key, (nama, harga) in data_menu.items():
-        print(f'{key}. {nama} - Rp. {harga}')
-    print('-'*30)
+        print(f'{key}. {nama} - Rp.{harga}')
+    print('-' * 30)
+
+
 # Fungsi Input pesanan / menyimpan pesanan
 daftar_pesanan = []
+
 def input_pesanan():
     while True:
         try:
-            pilih_menu = int(input('Massukkan nomor menu makan. ketik (0) jika sudah: '))
+            pilih_menu = int(input('Masukkan nomor menu (0 untuk selesai): '))
             
             if pilih_menu == 0:
                 break
             
             if pilih_menu not in data_menu:
-                print('Meu tidak tersedia')
+                print('Menu tidak tersedia')
                 continue
             
             jumlah_menu = int(input('Masukkan jumlah pesanan: '))
             
             if jumlah_menu <= 0:
-                print('Jumlah tidak boleh di bawah 0')
+                print('Jumlah harus lebih dari 0')
                 continue
             
+            daftar_pesanan.append((pilih_menu, jumlah_menu))
+        
         except ValueError:
             print('Input harus angka')
-        
-        daftar_pesanan.append((pilih_menu, jumlah_menu))
-# fungsi untuk menghitung total harga dari pesanan 
 
+
+# fungsi untuk menghitung total harga dari pesanan 
 def total_pesanan():
     print('\nTOTAL PESANAN\n')
     total_semua = 0
+
     for nomor, jumlah in daftar_pesanan:
         nama, harga = data_menu[nomor]
         total = jumlah * harga
@@ -61,47 +69,53 @@ def total_pesanan():
         
         print(f'{nama} x{jumlah} = Rp.{total}')
     
-    print('-'*30)
+    print('-' * 30)
     print(f'Total Semua: Rp.{total_semua}')
     return total_semua
+
 
 # fungsi input bayar
 def pembayaran(total):
     while True:
         try:
-            bayar = int(input('Bayar: '))
+            bayar = int(input(f'\nTotal yang harus di bayar: Rp.{int(total)}: '))
             
             if bayar >= total:
                 return bayar
-            
             else:
                 print('Uang tidak cukup')
+        
         except ValueError:
             print('Input harus angka')
+
+
 # fungsi hitung diskon
 def hitung_diskon(total):
-    if total >= 50000:
-        return total * 0.1
-    elif total >= 100000:
+    if total >= 100000:
         return total * 0.2
+    elif total >= 50000:
+        return total * 0.1
     else:
         return 0
-    
+
 
 # fungsi pajak
 def hitung_pajak(setelah_diskon):
     return setelah_diskon * 0.1
 
 
-#fungsi struk
-def struk(total, bayar, diskon, setelah_diskon, pajak, total_akhir):
+# fungsi struk
+def struk(total, bayar, diskon, setelah_diskon, pajak, total_akhir, waktu_transaksi):
     print('\nSTRUK PEMBAYARAN\n')
     
+    print(f'Tanggal : {waktu_transaksi.strftime("%d-%m-%Y")}')
+    print(f'Waktu : {waktu_transaksi.strftime("%H:%M:%S")}')
+    print('-'*30)
     for nomor, jumlah in daftar_pesanan:
         nama, harga = data_menu[nomor]
         total_item = jumlah * harga
         print(f'- {nama} x{jumlah} = Rp.{total_item}')
-        
+    
     print('-' * 30)
     print(f'Total           : Rp.{total}')
     print(f'Diskon          : Rp.{int(diskon)}')
@@ -112,7 +126,8 @@ def struk(total, bayar, diskon, setelah_diskon, pajak, total_akhir):
     print(f'Kembali         : Rp.{int(bayar - total_akhir)}')
     print('-' * 30)
     print('TERIMA KASIH SUDAH BELI\n')
-    
+
+
 # PROGRAM UTAMA
 while True:
     daftar_pesanan.clear()
@@ -131,10 +146,11 @@ while True:
     
     bayar = pembayaran(total_akhir)
     
-    struk(total, bayar, diskon, setelah_diskon, pajak, total_akhir)
+    waktu_sekarang = datetime.now()
+    
+    struk(total, bayar, diskon, setelah_diskon, pajak, total_akhir, waktu_sekarang)
     
     ulang = input('Transaksi lagi (y/n): ').lower()
     if ulang != 'y':
-        print('program selesai')
+        print('Program selesai')
         break
-    
